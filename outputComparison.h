3e8867fb32f84e2b8cd6e34e04cc12678fe8add0
DingKKK <623812325@qq.com>
2022-11-14 16:45:07 +0800

    output first commit

---
+++ outputComparison.h
@@ -0,0 +1,9 @@
+#define OUTPUT_COMPARISON
+#ifdef OUTPUT_COMPARISON
+#include<string>
+#include<vector>
+#include<iostream>
+using namespace std;
+void outputConversion(string filename, vector<string>& output);
+bool outputComparison(vector<string>& op1,vector<string>& op2);
+#endif
