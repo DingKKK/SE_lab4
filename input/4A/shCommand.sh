bcdcff4af97505046fc80d2e33e53802284e433a
DingKKK <623812325@qq.com>
2022-11-14 16:49:17 +0800

    testcase commit

---
+++ input/4A/shCommand.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+g++ -m64 /home/njucs/SE_lab/blog/input/4A/117364748.cpp -o /home/njucs/SE_lab/blog/input/4A/a.out
+chmod u+x /home/njucs/SE_lab/blog/input/4A/a.out
+/home/njucs/SE_lab/blog/input/4A/a.out </home/njucs/SE_lab/blog/input/4A/input.txt >/home/njucs/SE_lab/blog/input/4A/output.txt 2>&1
---
+++ input/50A/shCommand.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+g++ -m64 /home/njucs/SE_lab/blog/input/50A/36641065.cpp -o /home/njucs/SE_lab/blog/input/50A/a.out
+chmod u+x /home/njucs/SE_lab/blog/input/50A/a.out
+/home/njucs/SE_lab/blog/input/50A/a.out </home/njucs/SE_lab/blog/input/50A/input.txt >/home/njucs/SE_lab/blog/input/50A/output.txt 2>&1
