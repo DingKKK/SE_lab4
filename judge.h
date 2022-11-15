c7064b89e7fd92b6acf0171d12046fd5bbc0fa31
DingKKK <623812325@qq.com>
2022-11-15 11:15:48 +0800

    update branch judge

---
+++ judge.h
@@ -0,0 +1,11 @@
+#define JUDGE_H
+#ifdef JUDGE_H
+#include<vector>
+#include<string>
+#include<iostream>
+using namespace std;
+class Judge
+{
+public:
+    void judge(string path, string file1, string file2);
+};
